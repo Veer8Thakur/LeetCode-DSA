5bb96bbcd4bc2c68603fe95619eca2ca74a51018
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-06-24 18:43:25 +0530

    Time: 0 ms (100%), Space: 42.9 MB (74.87%) - LeetHub

--- 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.java
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.java
@@ -1,52 +1,31 @@
+
 class Solution {
+    private int min;
     public int minSessions(int[] tasks, int sessionTime) {
-        int n = tasks.length;
-        int totalMasks = 1 << n;
-
-        int[][] dp = new int[totalMasks][2];
-
-        for (int mask = 0; mask < totalMasks; mask++) {
-            dp[mask][0] = Integer.MAX_VALUE; // sessions
-            dp[mask][1] = Integer.MAX_VALUE; // load
+        this.min = tasks.length;
+        int[] sessions = new int[min];
+        Arrays.fill(sessions, sessionTime);
+        backtrack(tasks, sessions, min - 1, 0);
+        return min;
+    }
+    private void backtrack(int[] tasks, int[] sessions, int index, int sessionCount) {
+        if(sessionCount >= min) return;
+        if(index == -1) {
+            min = sessionCount;
+            return;
         }
 
-        dp[0][0] = 1; // start with one empty session
-        dp[0][1] = 0;
-
-        for (int mask = 0; mask < totalMasks; mask++) {
-
-            int sessions = dp[mask][0];
-            int load = dp[mask][1];
-
-            if (sessions == Integer.MAX_VALUE) continue;
-
-            for (int i = 0; i < n; i++) {
-
-                if ((mask & (1 << i)) != 0) continue;
-
-                int newMask = mask | (1 << i);
-
-                int newSessions;
-                int newLoad;
-
-                if (load + tasks[i] <= sessionTime) {
-                    newSessions = sessions;
-                    newLoad = load + tasks[i];
-                } else {
-                    newSessions = sessions + 1;
-                    newLoad = tasks[i];
-                }
-
-                if (newSessions < dp[newMask][0] ||
-                   (newSessions == dp[newMask][0] &&
-                    newLoad < dp[newMask][1])) {
-
-                    dp[newMask][0] = newSessions;
-                    dp[newMask][1] = newLoad;
-                }
+        int val = tasks[index];
+        for(int i = 0; i < sessionCount; i++) {
+            if(sessions[i] >= val) {
+                sessions[i] -= val;
+                backtrack(tasks, sessions, index - 1, sessionCount);
+                sessions[i] += val;
             }
         }
 
-        return dp[totalMasks - 1][0];
+        sessions[sessionCount] -= val;
+        backtrack(tasks, sessions, index - 1, sessionCount + 1);
+        sessions[sessionCount] += val;
     }
 }
