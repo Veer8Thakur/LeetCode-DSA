238bc8bdcb9e028b3150882cf5abdfa21828eec8
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-06-26 19:27:35 +0530

    Time: 143 ms (7.19%), Space: 45.9 MB (45.83%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,28 +1,45 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n = nums.length;
         Arrays.sort(nums);
-
+        int n = nums.length;
         List<List<Integer>> res = new ArrayList<>();
-        for(int i = 0; i<n-3; i++){
-            if(i > 0 && nums[i-1] == nums[i]) continue;
-            for(int j = i+1; j<n-2; j++){
-                if(j > i+1 && nums[j-1] == nums[j]) continue;
+
+        for(int i=0; i<n-3; i++){
+            if(i>0 && nums[i] == nums[i-1])
+                continue;
+
+            for(int j=i+1; j<n-2; j++){
+                if(j>i+1 && nums[j] == nums[j-1])
+                    continue;
+
                 int left = j+1, right = n-1;
+                long remainTarget = (long) target - nums[i] - nums[j];
+
                 while(left < right){
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
-                    if(sum == target){
+                    long sum = nums[left] + nums[right];
+
+                    if(sum == remainTarget){
                         res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        while(left < right && nums[left] == nums[left+1])
+                            left++;
+
+                        while(left < right && nums[right] == nums[right-1])
+                            right--;
+
                         left++;
                         right--;
-                        while(left < right && nums[left] == nums[left-1]) left++;
-                        while(left < right && nums[right] == nums[right+1]) right--;
                     }
-                    else if(sum < target) left++;
-                    else if(sum > target) right--;
+
+                    else if(sum < remainTarget)
+                        left++;
+
+                    else
+                        right--;
                 }
             }
         }
+
         return res;
     }
 }
