94e5c6253962b9c737c3938c5a2e74deb774dd46
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-06-26 19:27:46 +0530

    Time: 22 ms (21.08%), Space: 45.9 MB (31.23%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,45 +1,28 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
         int n = nums.length;
+        Arrays.sort(nums);
+
         List<List<Integer>> res = new ArrayList<>();
-
-        for(int i=0; i<n-3; i++){
-            if(i>0 && nums[i] == nums[i-1])
-                continue;
-
-            for(int j=i+1; j<n-2; j++){
-                if(j>i+1 && nums[j] == nums[j-1])
-                    continue;
-
+        for(int i = 0; i<n-3; i++){
+            if(i > 0 && nums[i-1] == nums[i]) continue;
+            for(int j = i+1; j<n-2; j++){
+                if(j > i+1 && nums[j-1] == nums[j]) continue;
                 int left = j+1, right = n-1;
-                long remainTarget = (long) target - nums[i] - nums[j];
-
                 while(left < right){
-                    long sum = nums[left] + nums[right];
-
-                    if(sum == remainTarget){
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if(sum == target){
                         res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-
-                        while(left < right && nums[left] == nums[left+1])
-                            left++;
-
-                        while(left < right && nums[right] == nums[right-1])
-                            right--;
-
                         left++;
                         right--;
+                        while(left < right && nums[left] == nums[left-1]) left++;
+                        while(left < right && nums[right] == nums[right+1]) right--;
                     }
-
-                    else if(sum < remainTarget)
-                        left++;
-
-                    else
-                        right--;
+                    else if(sum < target) left++;
+                    else if(sum > target) right--;
                 }
             }
         }
-
         return res;
     }
 }
