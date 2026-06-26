be192f7d346dc32324ce9c909e1006411981594c
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-06-26 18:21:40 +0530

    Time: 22 ms (99.89%), Space: 53 MB (99.93%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,45 +1,64 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
+     public List<List<Integer>> threeSum(int[] nums) {
+      List<List<Integer>> results = new ArrayList<>();
+        sort(nums);
+
         int n = nums.length;
+        for (int i = 0; i < n; i++){
+            if (i != 0 && nums[i] == nums[i-1]) continue;
+            if (nums[i] > 0) break;
 
-        for (int i = 0; i < n - 2; i++) {
+            int j = i+1;
+            int k = n-1;
 
-            // Skip duplicate first elements
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    left++;
-                    right--;
-
-                    // Skip duplicate second elements
-                    while (left < right && nums[left] == nums[left - 1]) {
-                        left++;
-                    }
-
-                    // Skip duplicate third elements
-                    while (left < right && nums[right] == nums[right + 1]) {
-                        right--;
-                    }
-
-                } else if (sum < 0) {
-                    left++;
+            while (j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                } else if (sum > 0){
+                    k--;
                 } else {
-                    right--;
-                }
+                    results.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+                    while (j < k && nums[j] == nums[j-1]) j++;                }
             }
         }
-        return res;
+        return results;
+    }
+
+    int[] aux;
+    private void sort(int[] arr) {
+        aux = new int[arr.length];
+        sort(arr, 0, arr.length);
+    }
+
+    private void sort(int[] arr, int low, int high) {
+        if (low+1 < high) {
+            int mid = low + (high-low)/2;
+
+            sort(arr, low, mid);
+            sort(arr, mid, high);
+
+            merge(arr, low, high);
+        }
+    }
+
+    private void merge(int[] arr, int low, int high) {
+        int l = low, mid = low + (high-low)/2, m = mid, n = high-low;
+
+        for (int i = 0; i < n; i++) {
+            if (l == mid) {
+                aux[i] = arr[m++];
+            } else if (m == high) {
+                aux[i] = arr[l++];
+            } else if (arr[l] <= arr[m]) {
+                aux[i] = arr[l++];
+            } else {
+                aux[i] = arr[m++];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[low+i] = aux[i];
+        }   
     }
 }
