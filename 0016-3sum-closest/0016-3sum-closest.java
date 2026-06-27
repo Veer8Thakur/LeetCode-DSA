70da944d4787c7fa9edb48f2408bca05433e9b36
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-06-27 19:12:47 +0530

    Time: 20 ms (12.75%), Space: 45.7 MB (30.15%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -5,7 +5,6 @@ class Solution {
         long closest = nums[0] + nums[1] + nums[2];
 
         for(int i = 0; i<n-2; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
             
             int left = i+1, right = n-1;  
             while(left < right){
