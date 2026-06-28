510e3afc3e1a265f1348f59f8fb2e9629844ffff
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-06-28 16:25:33 +0530

    Time: 9 ms (67.5%), Space: 77.2 MB (92.08%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -4,13 +4,12 @@ class Solution {
         int n = arr.length;
         if(n == 1) return 1;
         if(arr[0] != 1) arr[0] = 1;
-        int max = 1;
+        
         for(int i = 1; i<n; i++){
             if(Math.abs(arr[i] - arr[i-1]) > 1){
                 arr[i] = arr[i-1] + 1;
             }
-            max = Math.max(max, arr[i]);
         }
-        return max;
+        return arr[n-1];
     }
 }
