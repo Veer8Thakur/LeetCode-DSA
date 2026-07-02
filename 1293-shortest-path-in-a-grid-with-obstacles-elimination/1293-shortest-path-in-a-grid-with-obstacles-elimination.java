5ffed57e809a333cdf8a2fcaab66df9708d6bf49
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-07-02 19:09:18 +0530

    Time: 7 ms (91.46%), Space: 44.2 MB (95.65%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,54 +1,37 @@
 class Solution {
     public int shortestPath(int[][] grid, int k) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        // Optimization
+        int m = grid.length, n = grid[0].length;
         if (k >= m + n - 2) {
             return m + n - 2;
         }
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.offer(new int[]{0, 0, 0, 0});
+        
+        boolean[][][] vis = new boolean[m][n][k+1];
+        int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        while(!dq.isEmpty()){
+            int[] cur = dq.poll();
+            int r = cur[0], c = cur[1], wallBreak = cur[2], steps = cur[3];
+            if(r == m-1 && c == n-1) return steps;
+            vis[r][c][wallBreak] = true;
 
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][][] vis = new boolean[m][n][k + 1];
-
-        q.offer(new int[]{0, 0, 0, 0}); // row, col, wallsBroken, steps
-        vis[0][0][0] = true;
-
-        int[][] dirs = {
-            {-1, 0},
-            {1, 0},
-            {0, -1},
-            {0, 1}
-        };
-
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-
-            int r = cur[0];
-            int c = cur[1];
-            int walls = cur[2];
-            int steps = cur[3];
-
-            if (r == m - 1 && c == n - 1) {
-                return steps;
-            }
-
-            for (int[] d : dirs) {
-                int nr = r + d[0];
-                int nc = c + d[1];
-
-                if (nr < 0 || nr >= m || nc < 0 || nc >= n)
-                    continue;
-
-                int newWalls = walls + grid[nr][nc];
-
-                if (newWalls <= k && !vis[nr][nc][newWalls]) {
-                    vis[nr][nc][newWalls] = true;
-                    q.offer(new int[]{nr, nc, newWalls, steps + 1});
+            for(int[] dir: directions){
+                int newR = r + dir[0], newC = c + dir[1];
+                if(newR >= 0  && newR < m && newC >= 0 && newC < n && !vis[newR][newC][wallBreak]){
+                    int newWallBreak = wallBreak + grid[newR][newC];
+                    if(newWallBreak <= k && !vis[newR][newC][newWallBreak]){
+                        dq.offer(new int[]{newR, newC, newWallBreak, steps+1});
+                        vis[newR][newC][newWallBreak] = true;
+                    }
                 }
             }
         }
-
         return -1;
     }
 }
+
+
+
+
+
+
