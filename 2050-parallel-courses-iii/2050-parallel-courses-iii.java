9f8f514a21a081860d30ca9d4c769daea7e4ef73
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-07-03 18:33:33 +0530

    Time: 28 ms (46.99%), Space: 113.5 MB (50.3%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.java
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.java
@@ -9,41 +9,45 @@ class Solution {
             indegree[v]++;
         }
         int[] deg = indegree.clone();
+
         // TOPO SORT
         List<Integer> topo = new ArrayList<>(); // node
-        // Stack<Integer> topo = new Stack<>();
         Queue<Integer> q = new LinkedList<>(); // node
         
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MIN_VALUE);
+
         for(int i = 0; i<n; i++) {
-            if(indegree[i] == 0) q.offer(i);
+            if(indegree[i] == 0) {
+                q.offer(i);
+                dist[i] = time[i];
+            }
         }
         while(!q.isEmpty()){
             int node = q.poll();
             topo.add(node);
-            // topo.push(node);
             for(int nei: adj.get(node)) {
+                dist[nei] = Math.max(dist[nei], dist[node] + time[nei]);
                 indegree[nei]--;
                 if(indegree[nei] == 0) q.offer(nei);
             }
         }
 
         // DAG 
-
-        int[] dist = new int[n];
-        for(int i = 0; i<n; i++){
-            dist[i] = deg[i] == 0 ? time[i] : Integer.MIN_VALUE;
-        }
-        for(int cur: topo){
-            // int cur = topo.pop();
-            if(dist[cur] != Integer.MIN_VALUE){
-                for(int nei: adj.get(cur)){
-                    int cost = time[nei];
-                    if(dist[nei] < dist[cur] + cost) {
-                        dist[nei] = dist[cur] + cost;
-                    }
-                }
-            }
-        }
+        // int[] dist = new int[n];
+        // for(int i = 0; i<n; i++){
+        //     dist[i] = deg[i] == 0 ? time[i] : Integer.MIN_VALUE;
+        // }
+        // for(int cur: topo){
+        //     if(dist[cur] != Integer.MIN_VALUE){
+        //         for(int nei: adj.get(cur)){
+        //             int cost = time[nei];
+        //             if(dist[nei] < dist[cur] + cost) {
+        //                 dist[nei] = dist[cur] + cost;
+        //             }
+        //         }
+        //     }
+        // }
         int max = 0;
         for(int val : dist) max = Math.max(max, val);
         return max;
