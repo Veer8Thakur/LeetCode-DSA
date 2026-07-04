ec73069137350c4c29834880afb68aaee18b28d0
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-07-04 17:47:26 +0530

    Time: 31 ms (44.3%), Space: 159.5 MB (76.75%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -1,7 +1,8 @@
 class Solution {
-    int minScore = Integer.MAX_VALUE; 
+    int minScore;
     List<List<int[]>> adj;
     public int minScore(int n, int[][] roads) {
+        minScore = Integer.MAX_VALUE;
         adj = new ArrayList<>();
         for(int i = 0; i<=n; i++) adj.add(new ArrayList<>());
         for(int road[] : roads){
@@ -10,7 +11,7 @@ class Solution {
             adj.get(v).add(new int[]{u, w});
         }
         boolean vis[] = new boolean[n+1];
-        
+
         DFS(vis, 1);
         
         return minScore;
