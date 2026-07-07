89322e22a394b18da1a7406b2c66161bbcb1842b
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-07-07 17:38:38 +0530

    Time: 6 ms (99.62%), Space: 47.7 MB (73.68%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -4,23 +4,22 @@ class Solution {
         for(int pile : piles){
             high = Math.max(high, pile);
         }
-        int ans = 0;
+        
         while(low <= high){
             int mid = low + (high - low)/2;
             if(canEat(mid, piles, h)){
-                ans = mid;
                 high = mid-1;
             }
             else low = mid + 1;
         }
-        return ans;
+        return low;
     }
     public boolean canEat(int curCap, int[] piles, int h){
         long time = 0;
         for(int pile: piles){
-            time += ((long)pile + curCap - 1)/curCap; // ceil division
+            time += (pile + curCap - 1)/curCap; // ceil division
             if(time > h) return false;
         }
-        return true;
+        return time <= h;
     }
 }
