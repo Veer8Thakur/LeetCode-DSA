71df9cdf511decfcff54658c5fb78f709495cf9d
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-07-09 17:04:24 +0530

    Time: 3 ms (97.03%), Space: 163.2 MB (50.5%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
@@ -1,42 +1,26 @@
 class Solution {
-    int[] parent;
-    int[] rank;
+    public boolean[] pathExistenceQueries(
+        int n, int[] nums, int maxDiff, int[][] queries
+    ) {
+        int[] component = new int[n];
+        int id = 0;
 
-    public int find(int x){
-        if(parent[x] != x){
-            parent[x] = find(parent[x]);
-        }
-        return parent[x];
-    }
-
-    public void union(int x, int y){
-        int rootX = find(x);
-        int rootY = find(y);
-        if(rootX == rootY) return ;
-        if(rank[rootX] > rank[rootY]) parent[rootY] = rootX;
-        else if(rank[rootX] < rank[rootY]) parent[rootX] = rootY;
-        else {
-            parent[rootY] = rootX;
-            rank[rootX]++;
-        }
-    }
-
-    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
-        parent = new int[n];
-        rank = new int[n];
-        for(int i = 0; i<n; i++) parent[i] = i;
-
-        for(int i = 1; i<n; i++){
-            if(Math.abs(nums[i] - nums[i-1]) <= maxDiff) 
-                union(i-1, i); 
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] > maxDiff) {
+                id++;
+            }
+            component[i] = id;
         }
 
-        int m = queries.length;
-        boolean[] ans = new boolean[m];
-        for(int i = 0; i<m; i++){
-            int start = queries[i][0], end = queries[i][1];
-            if(find(start) == find(end)) ans[i] = true;
+        boolean[] ans = new boolean[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int u = queries[i][0];
+            int v = queries[i][1];
+
+            ans[i] = component[u] == component[v];
         }
+
         return ans;
     }
 }
