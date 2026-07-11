45819197354317837cbc454d2fc6e502d12c49ff
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-07-11 18:05:14 +0530

    Time: 2 ms (98.1%), Space: 43.1 MB (99%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -5,9 +5,13 @@ class Solution {
             map.put(num, map.getOrDefault(num, 0) + 1);
 
         Set<Integer> set = new HashSet<>();
-        for(Map.Entry<Integer, Integer> num: map.entrySet()) {
-            if(set.contains(num.getValue())) return false;
-            set.add(num.getValue());
+        // for(Map.Entry<Integer, Integer> num: map.entrySet()) {
+        //     if(set.contains(num.getValue())) return false;
+        //     set.add(num.getValue());
+        // }
+        for(var entry: map.entrySet()){
+            if(set.contains(entry.getValue())) return false;
+            set.add(entry.getValue());
         }
         return true;
     }
