7238046360d6c3941fc90142c362554438b7bcbd
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-07-11 17:01:55 +0530

    Time: 8 ms (94.15%), Space: 47.7 MB (39.49%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components-DFS.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int countCompleteComponents(int n, int[][] edges) {
+        List<Integer>[] adjList = new ArrayList[n];
+
+        for(int vertex = 0; vertex < n; vertex++) {
+            adjList[vertex] = new ArrayList<>();
+        }
+
+        for(int i=0; i<edges.length; i++) {
+            adjList[edges[i][0]].add(edges[i][1]);
+            adjList[edges[i][1]].add(edges[i][0]);
+        }
+
+        int counter = 0;
+        boolean[] visited = new boolean[n];
+        for(int i=0; i<n; i++) {
+            if(visited[i] == true) continue;
+            int[] componentInfo = new int[2];
+            dfs(adjList, visited, i, componentInfo);
+            if(componentInfo[0] * (componentInfo[0] - 1) == componentInfo[1]) counter++;
+            
+        }
+        return counter;
+
+    }
+
+    public void dfs(List<Integer>[] adjList, boolean[] visited, int ind, int[] componentInfo) {
+        visited[ind] = true;
+        componentInfo[0]++;
+        componentInfo[1] += adjList[ind].size();
+
+        for(int i=0; i<adjList[ind].size(); i++) {
+            int node = adjList[ind].get(i);
+            if(visited[node] == false) {
+                dfs(adjList, visited, node, componentInfo);
+            }
+        }
+    }
+}
