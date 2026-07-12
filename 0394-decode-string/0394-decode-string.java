07bb1394ed6041aa9f79ad940526c446465311df
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-07-12 21:52:41 +0530

    Time: 1 ms (86.38%), Space: 42.8 MB (72.71%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,28 +1,37 @@
 class Solution {
     public String decodeString(String s) {
-        char[] arr = s.toCharArray();
-
-        Stack<Character> stk = new Stack<>();
-        for(char c: arr){
-            if(c == ']'){
-                StringBuilder sb = new StringBuilder();
-                while(stk.peek() != '[') sb.append(stk.pop());
-                sb = sb.reverse();
-                stk.pop();
-                int k = 0;
-                StringBuilder digit = new StringBuilder(); 
-                while(!stk.isEmpty() && Character.isDigit(stk.peek())){
-                    digit.append(stk.pop());
-                }
-                digit.reverse();
-                k = Integer.parseInt(digit.toString());
-                for(int j = 0; j<k; j++)
-                    for(int i = 0; i<sb.length(); i++) stk.push(sb.charAt(i));
-            }
-            else stk.push(c);
-        }
+        Stack<Integer> numSt=new Stack<>();
+        Stack<String> strst = new Stack<>();
+        int num=0;
+        
         StringBuilder sb = new StringBuilder();
-        for(char c: stk) sb.append(c);
+
+        for(int i=0;i<s.length();i++){
+            if(Character.isDigit(s.charAt(i))){
+                num = num*10+(s.charAt(i)-'0');
+            }
+            
+            else if(s.charAt(i)=='['){
+                numSt.push(num);
+                strst.push(sb.toString());
+                num = 0;
+                sb=new StringBuilder();
+
+            }
+            else if(s.charAt(i)==']'){
+                int rep = numSt.pop();
+                String str = strst.pop();
+
+                StringBuilder temp = new StringBuilder(str);
+                for(int j=0;j<rep;j++){
+                    temp.append(sb);
+                }
+                sb = temp;
+            }
+            else{
+                sb.append(s.charAt(i));
+            }
+        }
         return sb.toString();
     }
 }
