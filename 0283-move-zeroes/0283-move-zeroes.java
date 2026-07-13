1cb167b901a3931d8e7e7dcaf32c133a51eb6cee
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-07-13 19:37:50 +0530

    Time: 4 ms (10.01%), Space: 47.9 MB (20.28%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -5,10 +5,10 @@ class Solution {
         for(int right = 0; right<n; right++){
             if(nums[left] == 0 && nums[right] != 0){
                 // b = a - b + (b = a);
-                // nums[right] = nums[left] - nums[right] + (nums[right] = nums[left]);
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
+                nums[right] = nums[left] - nums[right] + (nums[left] = nums[right]);
+                // int temp = nums[left];
+                // nums[left] = nums[right];
+                // nums[right] = temp;
                 left++;
             }
             if(nums[left] != 0) left++;
