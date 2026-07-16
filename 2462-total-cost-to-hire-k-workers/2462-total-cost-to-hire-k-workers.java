b698ae9d43f62e5103cdc331400b71d2aad714da
Veer8Thakur <134841687+Veer8Thakur@users.noreply.github.com>
2026-07-16 17:48:33 +0530

    Time: 52 ms (86.98%), Space: 72.9 MB (43.65%) - LeetHub

---
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
@@ -0,0 +1,46 @@
+class Solution {
+    public long totalCost(int[] costs, int k, int candidates) {
+        int n = costs.length;
+
+        PriorityQueue<Integer> leftPQ = new PriorityQueue<>();
+        PriorityQueue<Integer> rightPQ = new PriorityQueue<>();
+
+        int left = 0;
+        int right = n - 1;
+
+        // Fill left heap
+        while (left <= right && left < candidates) {
+            leftPQ.offer(costs[left++]);
+        }
+
+        // Fill right heap
+        while (left <= right && n - 1 - right < candidates) {
+            rightPQ.offer(costs[right--]);
+        }
+
+        long ans = 0;
+
+        while (k-- > 0) {
+
+            if (rightPQ.isEmpty() ||
+               (!leftPQ.isEmpty() && leftPQ.peek() <= rightPQ.peek())) {
+
+                ans += leftPQ.poll();
+
+                if (left <= right) {
+                    leftPQ.offer(costs[left++]);
+                }
+
+            } else {
+
+                ans += rightPQ.poll();
+
+                if (left <= right) {
+                    rightPQ.offer(costs[right--]);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
